35789e53c0d2665e304ed4ceb7e561779018610a
Antonio <antoniodaria@msn.com>
2018-11-29 14:45:02 +0000

    Refactored sign up test using web helpers

---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,8 @@
+def sign_up(user_email)
+  visit('/')
+  click_link('Sign up')
+  fill_in('user_email', with: user_email)
+  fill_in('user_password', with: 'password1234')
+  fill_in('user_password_confirmation', with: 'password1234')
+  click_button('Sign up')
+end
