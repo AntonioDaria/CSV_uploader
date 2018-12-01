c30582f6ed0838cf68918937e34c6c71cc7efe4a
Antonio <antoniodaria@msn.com>
2018-12-01 18:32:37 +0000

    Update production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  CarrierWave.configure do |config|
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:            ENV.fetch('AWS_REGION'), # Required
+      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+    }
+
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
+    config.aws_acl    = 'public-read'
+  end
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
